dd221f6ddd55182d02f6f9a09561027ef4837f7e
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-06-23 11:52:53 -0700

    lab

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,7 +1,20 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+const calculator = require("./calculate-back-end");
 
+ const numOne = getInput(0);
+ const numTwo = getInput(1);
+const op = getInput(2);
+
+
+
+
+
+let results = calculator(numOne, op, numTwo);
+
+
+console.log(results);
 
 
 
